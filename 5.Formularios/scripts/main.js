e5da0f49572a5261afa0130fd92ce59fbb904170
Nat209 <116594722+Nat209@users.noreply.github.com>
2023-03-07 08:59:51 -0500

    5.formularios

---
+++ 5.Formularios/scripts/main.js
@@ -0,0 +1,25 @@
+'use strict';
+/*
+seleccionar elementos desde el DOM
+*/
+
+var elemento1; //Global no recomendado para trabajar
+let elemento2; //Local es el recomendado
+const elemento3 = 3; //constantes- siempre debe tener un valor asignado( es una constante)
+
+const envio = document.getElementById("forma2");
+const formulario = document.getElementById("forma1");
+console.log(envio;)
+// asociando evento al boton seleccionado
+envio.addEventListener('click',(event)=>{
+    event.preventDefault();
+    console.log (formularios[0] value);
+    console.log (formularios[1] value);
+    console.log (formularios[2] value);
+    //console.log(),
+
+});
+
+
+
+
